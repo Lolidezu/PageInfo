963f8b72a81fdda6ebedb025b04ff9f74e17acb8
Lolidezu <aplbknooo@gmail.com>
2023-09-16 21:54:45 +0700

    Program added
    instead of webapp

--- Main.py
+++ Main.py
@@ -1,3 +1,8 @@
+from tkinter import font
+from tkinter import ttk
+import requests
+from bs4 import BeautifulSoup
+from tkinter import *
 import requests
 import time
 
@@ -83,6 +88,71 @@ def get_video_count(url):
     except requests.RequestException:
         return None
     
+def fetch_data():
+    url = url_entry.get()
+    fixed_url = "https://" + url
+
+    load_time = get_page_load_time(fixed_url)
+    ttfb = get_ttfb(fixed_url)
+    title = get_website_title(fixed_url)
+    pageSize = get_page_size(fixed_url)
+    imageCount = get_image_count(fixed_url)
+    videoCount = get_video_count(fixed_url)
+    
+    result_text.delete(1.0, "end")
+    if title is not None:
+        result_text.insert("end", "The page : " + title + " data is\n")
+    if load_time:
+        result_text.insert("end", f"The page loaded in {load_time:.6f} seconds.\n")
+    if ttfb is not None:
+        result_text.insert("end", f"The page get first byte in {ttfb:.6f} seconds.\n")
+    if pageSize is not None:
+        result_text.insert("end", f"The page size is : {pageSize:.6f} kilobytes.\n")
+    if imageCount is not None:
+        result_text.insert("end", "The page have : " + str(imageCount) + " images.\n")
+    if videoCount is not None:
+        result_text.insert("end", "The page have : " + str(videoCount) + " videos.\n")        
+    else:
+        result_text.insert("end", "Failed to fetch the page.\n")
+    
+
+try:
+    from ctypes import windll
+    windll.shcore.SetProcessDpiAwareness(1)
+except:
+    pass
+
+
+app = Tk()
+app.title("Website Info Fetcher")
+app.geometry("1280x720")
+
+custom_font = font.Font(family="Arial", size=12)
+url_label = Label(app, text="Enter website url:", font=custom_font)
+url_label.pack(pady=10)
+
+url_entry = Entry(app, width=40, font=custom_font)
+url_entry.pack(pady=10)
+
+button_image = PhotoImage(file="button_image.png")
+fetch_button = ttk.Button(app, command=fetch_data, image=button_image, compound="left")
+fetch_button.pack(pady=10)
+
+text_frame = Frame(app)
+text_frame.pack(pady=10)
+
+# Adjust the Text widget to be a child of the frame
+result_text = Text(text_frame, height=10, width=40, wrap='word', font=custom_font)
+result_text.pack(side='left', fill='both', expand=True)
+
+# Adjust the Scrollbar to be a child of the frame and bind it to the Text widget
+scrollbar = Scrollbar(text_frame, command=result_text.yview)
+scrollbar.pack(side='right', fill='y')
+result_text.config(yscrollcommand=scrollbar.set)
+
+app.mainloop()
+
+"""    
 url = input("Enter website url: ")
 fixed_url = "https://" + url
 load_time = get_page_load_time(fixed_url)
@@ -99,3 +169,4 @@ if ttfb is not None:
     print(f"The page get first byte in {load_time:.6f} seconds.")
 else:
     print("Failed to fetch the page.")
+"""
--- PSC/webchecker/forms.py
+++ PSC/webchecker/forms.py
@@ -2,3 +2,5 @@ from django import forms
 
 class UrlForm(forms.Form):
     url = forms.CharField(label='Enter website URL', max_length=2000)
+
+
