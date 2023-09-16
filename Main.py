42c70652a5970f07283cd975432136ba9a14cc9b
Lolidezu <aplbknooo@gmail.com>
2023-09-16 18:42:45 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,14 @@
 import requests
 import time
 
+def get_website_title(url):
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        return soup.title.string
+    except:
+        return None
+
 def get_page_load_time(url):
     start_time = time.time()
     
@@ -30,10 +38,58 @@ def get_ttfb(url):
         ttfb = end_time - start_time
         return ttfb
     
+def get_page_size(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            size_in_bytes = len(response.content)
+            size_in_kilobytes = size_in_bytes / 1024
+            return size_in_kilobytes
+    except requests.RequestException:
+        return None
+
+from bs4 import BeautifulSoup
+
+def get_text_amount(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            text = soup.stripped_strings
+            text_length = sum(len(item) for item in text)
+            return text_length
+    except requests.RequestException:
+        return None
+    
+def get_image_count(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            images = soup.find_all('img')
+            image_count = len(images) 
+            return image_count
+    except requests.RequestException:
+        return None
+
+def get_video_count(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            videos = soup.find_all('video')
+            video_count = len(videos)
+            return video_count
+    except requests.RequestException:
+        return None
+    
 url = input("Enter website url: ")
 fixed_url = "https://" + url
 load_time = get_page_load_time(fixed_url)
 ttfb = get_ttfb(fixed_url)
+title = get_website_title(fixed_url)
+if title is not None:
+    print("The page : " + title + " data is")
 if load_time:
     print(f"The page loaded in {load_time:.6f} seconds.")
 else:
